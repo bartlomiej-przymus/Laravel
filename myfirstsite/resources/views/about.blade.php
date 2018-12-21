08abd88e139b56582a0d6233df31965ad460c359
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2018-12-21 19:28:35 +0000

    First 6 lessons from laracasts podcast introducing laravel framework

---
+++ myfirstsite/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title', 'About Us')
+
+@section('content')
+    <h1>About Us</h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Hic odit tempore consequatur soluta molestias? Eveniet exercitationem,
+        necessitatibus deserunt quod, quam esse aut voluptate modi quos
+        maiores tempora nemo obcaecati beatae.
+    </p>
+
+@endsection
---
+++ myfirstsite/resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+    <h1>My First Website</h1>
+    <p>{{ $foo }}</p>
+@endsection
