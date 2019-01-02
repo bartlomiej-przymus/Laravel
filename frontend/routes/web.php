5edc552967f77ae0bfbf7768d149a18bd46b0af3
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2019-01-02 13:52:29 +0000

    added user flash message when for updated

--- frontend/routes/web.php
+++ frontend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,6 +11,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/', function (Request $request) {
+//     //$request->session()->put('foobar', 'baz');
+//     return $request->session()->get('foobar');
+// });
+
+Route::get('projects/create', function () {
+    return view('projects.create');
+});
+
+Route::post('projects', function () {
+    //validate project
+    //save project
+    session()->flash('message','Your project has been created'); //storing for single request
+    return redirect('/');
+});
 
 Route::get('/', function () {
     return view('welcome');
